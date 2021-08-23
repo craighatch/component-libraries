5717b28fd95f48233748d31e5b0db25e31c217fa
Craig Hatch <craig.hatch@credera.com>
2021-08-23 14:14:02 -0600

    initial styling of the home component

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,18 +1,34 @@
 import Header from "./header/header.component";
 import MainContent from "./main/main-content.component";
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    height: '100%',
+    flexDirection: 'column'
+  },
+  mainContent: {
+    flexGrow: 1,
+    background: 'linear-gradient(90deg, #706c6c, #ffffff)',
+  },
+  footer: {
+    minHeight: '50px'
+  }
+});
 
 const Home = (props) => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <div>
         <Header />
       </div>
-      <div>
+      <div className={classes.mainContent}>
         {/* <MainContent /> */}
         <div>main content area</div>
       </div>
-      <div>footer</div>
+      <div className={classes.footer}></div>
     </div>
   );
 }
